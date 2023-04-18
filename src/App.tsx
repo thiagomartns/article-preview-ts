adba70550bdc7923b1359bfdbc7f44b25872c8d6
thiago <tmg3.mn@gmail.com>
2023-04-18 11:27:59 -0300

    Add jsx and styles to root component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,36 @@
 import './App.scss';
+import Drawers from './assets/img/drawers.jpg'
+import Avatar from './assets/img/avatar-michelle.jpg'
+import ShareButton from './assets/img/icon-share.svg'
 
 function App() {
 
 
   return (
     <div className="App">
-      hello world
+      <div className="app-content">
+        <div className="app-content--img">
+          <img src={Drawers} alt="" />
+        </div>
+        <div className="app-content--info">
+          <div className="info-text">
+            <h1 className="info-h1">Shift the overall look and feel by adding these wonderful touches to furniture in your home</h1>
+            <p className="info-p">Ever been in a room and felt like something was missing? Perhaps it felt slightly bare and uninviting. I’ve got some simple tips to help you make any room feel complete.</p>
+          </div>
+          <div className="info-profile">
+            <div className="info-profile--user">
+              <img className='info-profile--avatar' src={Avatar} alt="" />
+              <div className="info-profile--username">
+                <h2 className="username">Michelle Appleton</h2>
+                <h3 className="date">28 jun 2020</h3>
+              </div>
+            </div>
+            <div className="info-profile--share">
+              <img className='image-share' src={ShareButton} alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
