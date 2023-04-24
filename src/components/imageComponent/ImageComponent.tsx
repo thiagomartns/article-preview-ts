7d6e0f9521a8d60f209e9d4012be5fbcc96049ea
thiago <tmg3.mn@gmail.com>
2023-04-24 13:53:00 -0300

    Remove Sass files

--- src/components/imageComponent/ImageComponent.tsx
+++ src/components/imageComponent/ImageComponent.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ImageComponentStyles from './styles';
 import Drawers from '../../assets/img/drawers.jpg'
-import './imageComponent.scss';
 
 const ImageComponent = () => {
   return (
